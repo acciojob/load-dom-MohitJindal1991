fa0d6b96b4218888432c7c1c906765bf9b045c85
MohitJindal1991 <90623594+MohitJindal1991@users.noreply.github.com>
2025-02-26 18:39:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded',()=>{
-	const  P=document.createElement('p');
-	P.innerHTML="DOM load success";
-	document.body.appendChild(P);
+	document.body.textContent("DOM load success");
 });
